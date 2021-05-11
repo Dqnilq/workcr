ed05daf6707eaecd559d19d35319e45d9208e62c
Danila Makarov <makarov01V@ya.ru>
2021-05-11 21:28:08 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -6,14 +6,13 @@ namespace controltest
     {
         static void Main(string[] args)
         {
-            // Клиентский код.
             var subject = new Another.Subject();
             var observerA = new ConcreteObserverA();
             subject.Attach(observerA);
 
             var observerB = new ConcreteObserverB();
             subject.Attach(observerB);
-
+            
             subject.SomeBusinessLogic();
             subject.SomeBusinessLogic();
 
